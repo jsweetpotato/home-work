3d3960f13df56f2724afbbd16a06b982086bae81
jsweetpotato37 <jsweetpotato37@gmail.com>
2023-12-09 01:21:48 +0900

    ninja 진짜 완성git add .git add .!

--- js-homework/mission-01/js/ninja.js
+++ js-homework/mission-01/js/ninja.js
@@ -1,9 +1,4 @@
-var [i1, i2] = document.querySelectorAll("input:nth-child(2)");
-
-console.log("i1: ", i1);
-console.log("i2: ", i2);
-
-var is = [];
+var i = document.querySelectorAll("input:nth-child(2)");
 
 const user = {
   id: "asd@naver.com",
@@ -48,14 +43,17 @@ let e = function (v) {
   }
 };
 
-var ಠ_ಠ;
+var p = "아이디";
+var ĩ = "비밀번호";
+
+var /* 이모지는 무조건 이쁜걸로 사용해야 변수명을 지을때 기분이 좋죠! */ ಠ_ಠ;
 var łļľ;
 
 function foo({ target: t }) {
-  if (t === i1) {
+  if (t === i[0]) {
     eventresult.call(t);
     is[0] = t.value === user.id;
-  } else if (t === i2) {
+  } else if (t === i[1]) {
     e(t);
     is[1] = t.value === user.pw;
   } else return;
@@ -63,12 +61,14 @@ function foo({ target: t }) {
 
 function logic(e) {
   e.preventDefault();
-  if (ಠ_ಠ && łļľ) {
-    if (is[0] && is[1]) {
-      window.location.href = "welcome.html";
-    } else alert("로그인에 실패하였습니다.");
-  }
+  ಠ_ಠ && łļľ
+    ? is[0] && is[1]
+      ? (window.location.href = "welcome.html")
+      : alert("로그인에 실패하였습니다.")
+    : alert(`${!ಠ_ಠ ? p : ĩ}를 잘못입력하였습니다.`);
 }
+
+var is = [];
 
 document.querySelector("form").addEventListener("input", foo);
 document.querySelector("form").addEventListener("submit", logic);
